75980c90e536cc3621380ad0a00bd767b8657750
solomonadzape <adzapesolomon@gmail.com>
2025-02-09 18:09:48 +0100

    fixed styled component prop errors

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -19,7 +19,7 @@ const FilterButton = styled.button`
   border: none;
 
   ${(props) =>
-    props.active &&
+    props.$active &&
     css`
       background-color: var(--color-brand-600);
       color: var(--color-brand-50);
@@ -50,7 +50,7 @@ export function Filter({ filterField, options }) {
         <FilterButton
           onClick={() => handleClick(option.value)}
           key={option.value}
-          active={searchParams.get("status") === option.value}
+          $active={searchParams.get("status") === option.value}
         >
           {option.label}
         </FilterButton>
--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -5,7 +5,7 @@ import { Select } from "./Select";
 /* eslint-disable react/prop-types */
 export function SortBy({ options }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const sortBy = searchParams.get("sortBy");
+  const sortBy = searchParams.get("sortBy") || "startDate-desc";
   function handleChange(e) {
     searchParams.set("sortBy", e.target.value);
     setSearchParams(searchParams);
